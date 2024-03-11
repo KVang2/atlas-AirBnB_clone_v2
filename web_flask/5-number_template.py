bca4a9beb9f354127dc7eb4198bba974f7498b12
kvang2 <kevinvaj88@hotmail.com>
2024-03-11 13:16:47 -0500

    fix num temp route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def display_number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def num_temp(HTML):
+def num_temp(n):
     return render_template('5-number.html', n=n)
 
 
