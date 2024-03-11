405b5ecde43b0b1b0077580c3a5503dc29f23ba5
kvang2 <kevinvaj88@hotmail.com>
2024-03-11 10:52:49 -0500

    added route for python text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Flask web application
+containing routes
+'/', '/hbnb', '/c/<text>, '/python/<text>'
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    return "Python" + text.replace('_'. ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
