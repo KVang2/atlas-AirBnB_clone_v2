17bf1228abf6e50f6952683df37dd78d0aeb9d04
kvang2 <kevinvaj88@hotmail.com>
2024-03-12 15:59:30 -0500

    add import state form model state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ removing current SQLAlchemy session after each request
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
